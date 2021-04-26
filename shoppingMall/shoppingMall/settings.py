4fc5761319384337f8cfe50a50ff703d99750961
dsaeun <daeun84366@gmail.com>
2021-04-26 22:29:52 +0900

    update gitingnore

--- shoppingMall/shoppingMall/settings.py
+++ shoppingMall/shoppingMall/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'admin',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '1',
         'HOST': 'localhost',
         'PORT': '3306',
     }
