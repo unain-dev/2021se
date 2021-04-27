d565286198d5c84454404b410c730c565a9d5e68
dsaeun <daeun84366@gmail.com>
2021-04-27 14:44:14 +0900

    revert

--- shoppingMall/shoppingApp/models.py
+++ shoppingMall/shoppingApp/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class UserAccounts(models.Model):
-    user_id = models.CharField(max_length=100)
-    user_pw = models.CharField(max_length=100)
-    user_address = models.TextField()
-    user_email = models.CharField(max_length=100)
-    user_phone = models.IntegerField(null=True)
-
-    def __str__(self):
-       return self.user_id
--- shoppingMall/shoppingApp/views.py
+++ shoppingMall/shoppingApp/views.py
@@ -1,24 +1,3 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import UserAccounts
+from django.shortcuts import render
 
 # Create your views here.
-def signUp(request):
-    return render(request, "signUp.html")
-
-def hello(request, id):
-    useraccounts=get_object_or_404(UserAccounts, pk=id)
-    return render(request, "hello.html", {'useraccounts':useraccounts})
-
-def test(request):
-    useraccounts=UserAccounts.objects.all()
-    return render(request, "test.html", {'useraccounts' : useraccounts})
-
-def createUser(request):
-    new_userAccounts=UserAccounts()
-    new_userAccounts.user_id = request.POST['new_user_id']
-    new_userAccounts.user_pw = request.POST['new_user_pw']
-    new_userAccounts.user_address = request.POST['new_user_address']
-    new_userAccounts.user_email = request.POST['new_user_email']
-    new_userAccounts.user_phone = request.POST['new_user_phone']
-    new_userAccounts.save()
-    return redirect('hello', new_userAccounts.id)
