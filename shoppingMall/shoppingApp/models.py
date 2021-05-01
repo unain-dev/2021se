39c43e2340b115bed7b9fa298c8e9a8176aaf994
Sangmin Byeon <91j6j5@naver.com>
2021-05-02 00:00:27 +0900

    add 'notice', 'event' in models.py

--- shoppingMall/shoppingApp/models.py
+++ shoppingMall/shoppingApp/models.py
@@ -11,3 +11,22 @@ class UserAccounts(models.Model):
 
 def __str__(self):
     return self.user_id
+
+
+#공지, 이벤트 - 상민
+class Notice(models.Model):
+    title = models.CharField(max_length=50)
+    createDate = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    contents = models.TextField()
+    uniqueNumber = models.IntegerField()
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    createDate = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    contents = models.TextField()
+    on_off = models.TextChoices('on','off')
+    activation = models.CharField(choices=on_off.choices, max_length=10)
+    dueDate = models.DateTimeField()
+    uniqueNumber = models.IntegerField()    
