a5ebb17b1b772945f00c2885b7548200e457baa0
dsaeun <daeun84366@gmail.com>
2021-05-26 02:17:19 +0900

    edit database

--- shoppingMall/shoppingMall/settings.py
+++ shoppingMall/shoppingMall/settings.py
@@ -80,14 +80,24 @@ WSGI_APPLICATION = 'shoppingMall.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': os.environ.get('DB_NAME'),
+       'USER': os.environ.get('DB_USER'),
+       'PASSWORD': os.environ.get('DB_PASSWORD'),
+       'HOST': os.environ.get('DB_HOST'),
+       'PORT': '',
+   }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
