3c3b9a75704197f2d3d9a18a6c40bd8dfaa2f96e
dsaeun <daeun84366@gmail.com>
2021-05-26 10:26:29 +0900

    edit wsgi

--- shoppingMall/shoppingMall/wsgi.py
+++ shoppingMall/shoppingMall/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoppingMall.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoppingMall.shoppingMall.settings')
 
 application = get_wsgi_application()
