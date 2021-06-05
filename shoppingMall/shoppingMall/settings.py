aa33089251b8262b7f4a2a634e787faaac4d01ab
dsaeun <daeun84366@gmail.com>
2021-06-05 22:18:40 +0900

    change db

--- shoppingMall/shoppingMall/settings.py
+++ shoppingMall/shoppingMall/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': os.environ.get('DB_NAME'),
-       'USER': os.environ.get('DB_USER'),
+       'NAME': 'shop',
+       'USER': 'admin',
        'PASSWORD': os.environ.get('DB_PASSWORD'),
        'HOST': os.environ.get('DB_HOST'),
        'PORT': '',
